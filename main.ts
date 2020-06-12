e2f1f42133e7f223e4a3112ae0a5f895a9e21305
Gengar9nn <66795330+Gengar9nn@users.noreply.github.com>
2020-06-12 17:12:13 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,177 @@
+namespace myTiles {
+    //% blockIdentity=images._tile
+    export const tile0 = img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`
+    //% blockIdentity=images._tile
+    export const tile1 = img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 2 4 4 4 2 2 2 4 4 4 2 2 2 4 4 
+4 2 4 4 4 4 4 2 4 4 4 4 4 2 4 4 
+4 2 4 4 4 2 2 2 4 4 4 2 2 2 4 4 
+4 2 4 4 4 2 4 4 4 4 4 4 4 2 4 4 
+4 2 4 4 4 2 2 2 4 4 4 2 2 2 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 2 4 4 4 4 4 4 4 4 4 4 2 2 4 4 
+4 2 4 4 2 4 4 2 2 4 4 4 2 4 4 4 
+4 2 2 2 2 2 4 2 4 4 4 4 2 4 4 4 
+4 4 4 4 2 4 4 2 2 2 4 4 2 2 2 4 
+4 4 4 4 2 4 4 4 4 2 4 4 2 4 2 4 
+4 4 4 4 4 4 4 2 2 2 4 4 2 2 2 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`
+    //% blockIdentity=images._tile
+    export const tile4 = img`
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 4 
+`
+    //% blockIdentity=images._tile
+    export const tile5 = img`
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`
+    //% blockIdentity=images._tile
+    export const tile6 = img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 f f f f 1 f f f f f 1 1 1 1 
+1 1 f 1 1 f 1 f 1 1 1 f 1 1 1 1 
+1 1 f 1 1 f 1 f f f f f 1 1 1 1 
+1 1 f f f f 1 f 1 1 1 f 1 1 1 1 
+1 1 f 1 1 1 1 f 1 1 1 f 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 f f f f f 1 1 f f f f 1 1 1 1 
+1 f 1 1 1 1 1 1 f 1 1 1 1 1 1 1 
+1 f f f f f 1 1 f f f f 1 1 1 1 
+1 1 1 1 1 f 1 1 1 1 1 f 1 1 1 1 
+1 f f f f f 1 1 f f f f 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`
+    //% blockIdentity=images._tile
+    export const tile7 = img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 f 1 1 1 1 1 1 f f f f 1 1 1 1 
+1 f 1 f 1 1 1 1 f 1 1 1 1 1 1 1 
+1 f 1 f 1 1 1 1 f f f f 1 1 1 1 
+1 f f f f f 1 1 1 1 1 f 1 1 1 1 
+1 1 1 f 1 1 1 1 f f f f 1 1 1 1 
+1 1 1 f 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+`
+}
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile1, function (sprite, location) {
+    Actual_password = 45
+    Input_password = game.askForNumber("What’s the pass?", 2)
+    if (Actual_password == Input_password) {
+        tiles.placeOnRandomTile(mySprite, myTiles.tile5)
+    } else {
+        Actual_password = Math.randomRange(0, 9)
+        Input_password = game.askForNumber("What’s the pass?", 2)
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile4, function (sprite, location) {
+    game.over(true, effects.smiles)
+})
+let Input_password = 0
+let Actual_password = 0
+let mySprite: Sprite = null
+tiles.setTilemap(tiles.createTilemap(
+            hex`100010000f100104040401010101010101010101010101040d040101010101010101010101010104010104040404040404010101010101040101010101010101040101010101010401010101010101010401010101010104010404040404010104010101010101040104010101040101040101010101010401040101010401010401010101010104010401010404010104010101010101040104010104010401040101010101010401040101040104010401010101010104010401010401040e0504010101010104040401010401040404040101010101010101010101010101010101010101010101010101010101010101010103010101010101010101010101010101`,
+            img`
+. . . 2 2 2 . . . . . . . . . . 
+. . . 2 . 2 . . . . . . . . . . 
+. . . 2 . . 2 2 2 2 2 2 2 . . . 
+. . . 2 . . . . . . . . 2 . . . 
+. . . 2 . . . . . . . . 2 . . . 
+. . . 2 . 2 2 2 2 2 . . 2 . . . 
+. . . 2 . 2 . . . 2 . . 2 . . . 
+. . . 2 . 2 . . . 2 . . 2 . . . 
+. . . 2 . 2 . . 2 2 . . 2 . . . 
+. . . 2 . 2 . . 2 . 2 . 2 . . . 
+. . . 2 . 2 . . 2 . 2 . 2 . . . 
+. . . 2 . 2 . . 2 . 2 . . . . . 
+. . . 2 2 2 . . 2 . 2 2 2 . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`,
+            [myTiles.tile0,sprites.castle.tilePath5,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.builtin.brick,myTiles.tile1,sprites.castle.shrub,sprites.builtin.crowd8,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleInsignia,sprites.builtin.crowd3,sprites.builtin.crowd7,sprites.castle.saplingOak,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7],
+            TileScale.Sixteen
+        ))
+mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . 2 2 2 2 2 2 2 2 2 . . . . 
+. . . 2 2 2 1 f 1 2 2 2 . . . . 
+. . . 2 2 2 f 1 f 2 2 2 . . . . 
+. . . 2 2 2 2 2 2 2 2 2 2 2 . . 
+. . . . d 1 d d 1 d d . . . . . 
+. . . . d d d d d d d . . . . . 
+. . . . d f f f f d d . . . . . 
+. . . . d d d d d d d . . . . . 
+. . . 2 8 8 8 8 8 8 8 2 . . . . 
+. . 2 . 8 5 8 8 8 5 8 . 2 . . . 
+. 2 . . 8 8 8 8 8 8 8 . . 2 . . 
+. . . . 8 8 8 8 8 8 8 . . . . . 
+. . . . f . . . . . f . . . . . 
+. . . f . . . . . . . f . . . . 
+`, SpriteKind.Player)
+tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath8)
+controller.moveSprite(mySprite)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(25)
